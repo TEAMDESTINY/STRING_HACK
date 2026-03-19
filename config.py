5046cfcfac0aa785d851cb254fbbc6eaea226ed3
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-19 15:15:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
     API_HASH = os.getenv("API_HASH", "c81ae4c3dc026ea4bf49842a8ce4a5f9")
     TOKEN = os.getenv("TOKEN", None)
     MONGO_URL = os.getenv("MONGO_URL", None)
-    OWNER_ID = int(os.getenv("OWNER_ID", "7473021518"))
-    LOGGER_GROUP_ID = int(os.getenv("LOGGER_GROUP_ID", "-1003055975764"))
+    OWNER_ID = int(os.getenv("OWNER_ID", "8268923347"))
+    LOGGER_GROUP_ID = int(os.getenv("LOGGER_GROUP_ID", "-1003778506343"))
     START_PIC = os.getenv("START_PIC", "https://files.catbox.moe/ppvvg0.jpg")
